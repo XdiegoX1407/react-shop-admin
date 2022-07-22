825a5165c06722fd9ec53c8cfb5199da87d82cba
Diego Cotte <diegocotte1407@gmail.com>
2022-07-22 17:41:01 -0500

    add pagination

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+let colors = require('tailwindcss/colors');
+
+delete colors['lightBlue'];
+delete colors['warmGray'];
+delete colors['trueGray'];
+delete colors['coolGray'];
+delete colors['blueGray'];
+colors = { ...colors, ...{ transparent: 'transparent' } };
+
+module.exports = {
+  content: ['./src/**/*{html,js,jsx}'],
+  theme: {
+    colors: {
+      ...colors,
+    },
+  },
+  plugins: [],
+};
